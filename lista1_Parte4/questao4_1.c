c511c70be7dcadefa3a1f72f23f972a9772b1666
Caio Navarro <caionavarrof@gmail.com>
2026-08-18 17:57:26 -0300

    add atividade 4

--- lista1_Parte4/questao4_1.c
+++ lista1_Parte4/questao4_1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int maior(int a, int b, int c){
+    if(a > b && a > c)
+        return a;
+    else if(b > c)
+        return b;
+    else
+        return c;
+}
+
+int main(){
+    int n1, n2, n3;
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+    printf("Digite o terceiro numero: ");
+    scanf("%d", &n3);
+    printf("Maior: %d\n", maior(n1, n2, n3));
+    return 0;
+}
