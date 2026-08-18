0c7a81d7db577c82dce8184d487266c0b9ec572e
CaioNavarroF <caionavarrof@gmail.com>
2026-08-18 13:15:19 -0300

    Add questao1_1.c to lista_Parte1

---
+++ lista_Parte1/questao1_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+    printf("Soma: %d\n", n1 + n2);
+    return 0;
+}
