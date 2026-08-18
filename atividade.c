baa0f016fb2e63a1328a7708533370a27d076a14
CaioNavarroF <caionavarrof@gmail.com>
2026-08-18 12:55:27 -0300

    Add files via upload

---
+++ atividade.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    
+    int x, y;
+    char op;
+    printf("Digite um valor: ");
+    scanf("%d", &x);
+    printf("Digite a operacao: ");
+    scanf(" %c", &op);
+    printf("Digite o segundo valor: ");
+    scanf("%d", &y);
+    if(op == '+'){
+        printf("Resultado: %d\n", x + y);
+    }
+    else if(op == '-'){
+        printf("Resultado: %d\n", x - y);
+    }
+    else if(op == '*'){
+        printf("Resultado: %d\n", x * y);
+    }
+    else if(op == '/'){
+        if(y == 0){
+            printf("Nao e possivel dividir por zero!\n");
+            return 0;
+        }
+        printf("Resultado: %d\n", x / y);
+    }
+    else{
+        printf("Operacao invalida!\n");
+    }
+    return 0;
+}
